314ab1b3131dce07bd83d4c77cb6a90005b31e88
Sunny You <sunny.jiang.you@gmail.com>
2025-01-13 21:28:00 -0500

    Removed python debugger.

--- lua/custom/mappings.lua
+++ lua/custom/mappings.lua
@@ -1,26 +1,5 @@
 local M = {}
 
-M.dap = {
-    plugin = true,
-    n = {
-        ["<leader>db"] = {
-            "<cmd> DapToggleBreakpoint <CR>", "Breakpoint"
-        }
-    }
-}
-
-M.dap_python = {
-    plugin = true,
-    n = {
-        ["<leader>dr"] = {
-            function()
-                require("dap-python").test_method()
-            end,
-            "Run Test Method"
-        }
-    }
-}
-
 M.gitsigns = {
     plugin = true,
     n = {
